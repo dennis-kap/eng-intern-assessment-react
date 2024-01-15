59398857d9253ca08c4955040666bcd30c389824
dennis-kap <102987428+dennis-kap@users.noreply.github.com>
2024-01-14 23:27:34 -0500

    created stopwatch times and start/stop button

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react';
 
-export default function StopWatch() {
+function TwoDigit(n: number) {
+    return n > 9 ? "" + n: "0" + n;
+}
+
+const Stopwatch = () => {
+    const [isRunning, setRunning] = useState(false);
+    const [totalSeconds, setSeconds] = useState(0);
+
+    function toggleRunning() {
+        setRunning(!isRunning);
+    }
+
+    var hrs = Math.floor(totalSeconds / 3600);
+    var mins = Math.floor((totalSeconds % 3600) / 60);
+    var secs = totalSeconds % 60;
+
+    var hrsStr = TwoDigit(hrs);
+    var minsStr = TwoDigit(mins);
+    var secsStr = TwoDigit(secs);
+
     return(
-        <div></div>
+        <div>
+            <h2>Stopwatch - Dennis K</h2>
+
+            <h1>{hrsStr}:{minsStr}:{secsStr}</h1>
+
+            <button onClick={toggleRunning}>
+                {isRunning ? 'Stop' : 'Start'}
+            </button>
+            
+            <button></button>
+        </div>
+
     )
 }
+
+export default Stopwatch;
